d2322d6dd53333af6ffd07df5a4c41814040e297
zeromuo <115249896+zeromuo@users.noreply.github.com>
2023-02-19 15:47:54 +0800

    Update 未命名7.cpp

--- studying in school/lesson_2/未命名7.cpp
+++ studying in school/lesson_2/未命名7.cpp
@@ -1,30 +1,28 @@
-#include<iostream> 
+#include<iostream>
 #include<cstring>
 using namespace std;
-float fun(float a,float b,float c){
-    if(a<b){
-                if(c>b){
-                    return c;	
-                }
-                if(c<b){
-                    return b;	
-                }
-            }
-    if(a>b){
-            if(c>a){
-            	return c;	
-            }
-            if(c<a){
-            	return a;	
-            }
-            }
+float fun(float a, float b, float c) {
+	if (a < b) {
+		if (c > b) {
+			return c;
+		}
+		if (c < b) {
+			return b;
+		}
+	}
+	if (a > b) {
+		if (c > a) {
+			return c;
+		}
+		if (c < a) {
+			return a;
+		}
+	}
 }
-int main(){
-	int o,p,q,tmp;
+int main() {
+	int o, p, q, tmp;
 	float op;
-	cin>>o>>p>>q;
-	tmp=fun(o+p,o,p)*fun(o,p,p+q);
-    op=fun(o,p,q)/tmp;
-    cout<<op;
-	
-}
+	cin >> o >> p >> q;
+	tmp = fun(o + p, o, p) * fun(o, p, p + q);
+	op = fun(o, p, q) / tmp;
+	cout << op;
