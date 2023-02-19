3132b8d1f2223572965468edd31b7b566579e81b
zeromuo <115249896+zeromuo@users.noreply.github.com>
2023-02-19 15:37:20 +0800

    Update 未命名6.cpp

--- studying in school/lesson_2/未命名6.cpp
+++ studying in school/lesson_2/未命名6.cpp
@@ -1,17 +1,21 @@
-#include<iostream> 
+#include<iostream>
 #include<cstring>
 using namespace std;
-int main(){
-	int n,i,j,op,temp;
-	cin>>n;
-	op=0;
-	for(j=1;j<=n;j++){
-		temp=1;
-		for(i=1;i<=j;i++){
-				temp=temp*i;
-				
-			}
-		op=op+temp;
+long long fun(int j) {
+	int i;
+	long long temp = 1;
+	for (i = 1; i <= j; i++) {
+		temp = temp * i;
 	}
-	cout<<op<<endl;
+	return temp;
+
+}
+int main() {
+	int n, j;
+	long long op = 0;
+	cin >> n;
+	for (j = 1; j <= n; j++) {
+		op = op + fun(j);
+	}
+	cout << op;
 }
