824f188b02efd45fc7fc77b195c1e9e1683a6405
zeromuo <115249896+zeromuo@users.noreply.github.com>
2023-02-19 15:45:09 +0800

    Add files via upload

---
+++ studying in school/lesson_2/未命名7.cpp
@@ -0,0 +1,30 @@
+#include<iostream> 
+#include<cstring>
+using namespace std;
+float fun(float a,float b,float c){
+    if(a<b){
+                if(c>b){
+                    return c;	
+                }
+                if(c<b){
+                    return b;	
+                }
+            }
+    if(a>b){
+            if(c>a){
+            	return c;	
+            }
+            if(c<a){
+            	return a;	
+            }
+            }
+}
+int main(){
+	int o,p,q,tmp;
+	float op;
+	cin>>o>>p>>q;
+	tmp=fun(o+p,o,p)*fun(o,p,p+q);
+    op=fun(o,p,q)/tmp;
+    cout<<op;
+	
+}
