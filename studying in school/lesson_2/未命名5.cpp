01f54c5ca401ad636b8dfe848f2000672f95fa83
zeromuo <115249896+zeromuo@users.noreply.github.com>
2023-02-19 15:03:55 +0800

    Update 未命名5.cpp

--- studying in school/lesson_2/未命名5.cpp
+++ studying in school/lesson_2/未命名5.cpp
@@ -1,23 +1,43 @@
+//using namespace std;
+//void fun(int a,int b){
+//	for(int i=0;i<a;i++){
+//		    cin>>b;
+//		    bool flag=true;
+//			for(int j=2;j<=sqrt(b);j++){
+//			   	if(b%j==0){
+//					flag=false;
+//		    		break;
+//			    	}
+//			}
+//			if(flag){
+//					cout<<b<<" ";
+//		}
+//		}
+//}
+//int main(){
+//	int c,d;
+//	cin>>c;
+//	fun(c,d);
+	
+//}
 #include<bits/stdc++.h>
 using namespace std;
-void fun(int a,int b){
-	for(int i=0;i<a;i++){
-		    cin>>b;
-		    bool flag=true;
-			for(int j=2;j<=sqrt(b);j++){
-			   	if(b%j==0){
-					flag=false;
-		    		break;
-			    	}
+bool isprime(int a){
+		for(int j=2;j<=sqrt(a);j++){
+		   	if(a%j==0){
+		    	return false;
+			    }
 			}
-			if(flag){
-					cout<<b<<" ";
-		}
-		}
+			return true;
 }
 int main(){
-	int c,d;
-	cin>>c;
-	fun(c,d);
+	int n;
+	cin>>n;
+	int a[n];
+	for(int i=0;i<5;i++){
+		cin>>a[i];
+		if(isprime(a[i]))
+			cout<<a[i]<<" ";
+	}
 	
 }
