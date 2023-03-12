73c93117864f565049368fb751f9e459b3b2503c
zeromuo <115249896+zeromuo@users.noreply.github.com>
2023-03-12 14:35:30 +0800

    Add files via upload

--- studying in school/lesson_6/未命名1.cpp
+++ studying in school/lesson_6/未命名1.cpp
@@ -1,16 +1,11 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
-int fun(int n){
-	if(n==1){
-		return 0;
-	}
-	if(n==2){
-		return 1;
-	}
-	return fun(n-1)+fun(n-2);
-}
 int main(){
-	int c;
-	cin>>c;
-	cout<<fun(c+2);
+	int a,b;
+	cin>>a>>b;
+	int *p=NULL;
+	int *q=NULL;
+	p=&b;
+	q=&a;
+	cout<<*p<<" "<<*q;
 }
